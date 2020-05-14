2d6c0f62a25ff9cfee9e17c99dc39e96412c65d4
Subilan <suyuanliang1995@gmail.com>
2020-05-14 10:47:11 +0800

    Main class with yaml loader

---
+++ src/main/java/win/subilan/Ageing/Ageing.java
@@ -0,0 +1,42 @@
+package win.subilan.Ageing;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Ageing extends JavaPlugin {
+    FileConfiguration ageData;
+
+    @Override
+    public void onEnable() {
+        this.saveDefaultConfig();
+        this.ageData = this.load("age.yml");
+        this.getLogger().info(G.translateColor(G.success + "The plugin has been &aenabled&r."));
+        Bukkit.getPluginCommand("age").setExecutor(new CommandHandler(this));
+    }
+
+    @Override
+    public void onDisable() {
+        this.getLogger().info(G.translateColor(G.success + "The plugin has been &cdisabled&r."));
+    }
+
+    public FileConfiguration load(String filename) {
+        File folder = this.getDataFolder();
+        File file = new File(folder, filename);
+        if (!folder.exists()) {
+            folder.mkdir();
+        }
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return YamlConfiguration.loadConfiguration(file);
+    }
+}
