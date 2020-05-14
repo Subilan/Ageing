ca21d77ce2260459377f67452cb7fb8618767c8a
Subilan <suyuanliang1995@gmail.com>
2020-05-14 12:13:34 +0800

    Fix blockstatement, add reload and save functions

--- src/main/java/win/subilan/Ageing/CommandHandler.java
+++ src/main/java/win/subilan/Ageing/CommandHandler.java
@@ -14,26 +14,31 @@ public class CommandHandler implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        if (cmd.getName().equalsIgnoreCase("age") {
+        if (cmd.getName().equalsIgnoreCase("age")) {
             String arg = args[0];
             FileConfiguration config = this.plug.getConfig();
+            FileConfiguration ageData = this.plug.ageData;
 
             switch (arg) {
                 case "set":
-                    this.plug.ageData.set(args[1], args[2]);
+                    ageData.set(args[1], args[2]);
                     sender.sendMessage(G.translateColor(G.success + "Successfully set &a" + args[1] + "&r's age to &a" + args[2] + "&r."));
                     break;
 
                 case "get":
-                    String age = this.plug.ageData.getString(args[1]);
+                    String age = ageData.getString(args[1]);
                     sender.sendMessage(G.translateColor(G.info + "The age of &a" + args[1] + "&r is &a" + age + "&r."));
                     break;
                 
                 case "reload":
                     this.plug.reloadConfig();
+                    this.plug.reloadData();
                     sender.sendMessage(G.translateColor(G.success + "Successfully reload the plugin configuration"));
                     break;
             }
+
+            this.plug.saveConfig();
+            this.plug.saveData();
         }
         return false;
     }
